7c0d7ba97113b94233dd169d83bf5c0f8921ca4b
Ramganga <ram886694@gmail.com>
2025-03-21 19:50:20 +0530

    21032025

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
-import pureComp from './component/pureComp'
+import ClickCounterTwo from "./component/ClickCounterTwo";
+import Counter2 from "./component/Counter2";
+import HoverCounterTwo from "./component/HoverCounterTwo";
+
+
 function App() {
   return (
     <div className="App">
-      <pureComp />
+      <Counter2
+        render={(count, incrementCount) => (
+          <ClickCounterTwo count={count} incrementCount={incrementCount} />
+        )}
+      />
+
+      <Counter2
+        render={(count, incrementCount) => (
+          <HoverCounterTwo count={count} incrementCount={incrementCount} />
+        )}
+      />
+
+      
     </div>
   );
 }
