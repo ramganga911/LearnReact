302e658554fe463b140924b2870c85643fda718d
Ramganga <ram886694@gmail.com>
2025-03-22 01:58:08 +0530

    22-03-2025

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import "./App.css";
-import ClickCounterTwo from "./component/ClickCounterTwo";
-import Counter2 from "./component/Counter2";
-import HoverCounterTwo from "./component/HoverCounterTwo";
-
 
 function App() {
   return (
     <div className="App">
-      <Counter2
-        render={(count, incrementCount) => (
-          <ClickCounterTwo count={count} incrementCount={incrementCount} />
-        )}
-      />
-
-      <Counter2
-        render={(count, incrementCount) => (
-          <HoverCounterTwo count={count} incrementCount={incrementCount} />
-        )}
-      />
-
-      
+      <h1>Thinking = Action = Habbit = Lifestyle  </h1>
+      <h1>Destiny</h1>
     </div>
   );
 }
